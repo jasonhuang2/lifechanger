2d2b3aaea377e39ac4e06028229b4038830053a0
Jason Huang <jasonhuang@Jasons-MacBook-Air.local>
2026-04-02 13:25:52 -0600

    first commit base spring

---
+++ src/test/java/com/jasonhuang/lifechanger/LifechangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonhuang.lifechanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifechangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
